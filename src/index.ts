e40b53bb582883cddfee2d7485f5480d49415db9
Anthony Fu <github@antfu.me>
2024-07-18 15:05:53 +0200

    chore: update

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
+/* eslint-disable no-console */
 import type { Plugin } from 'vite'
 import { scanPackagesList } from './scan'
 
-function OptimizeExclude(): Plugin {
+export interface OptimizeExcludeOptions {
+  debug?: boolean
+}
+
+function OptimizeExclude(options: OptimizeExcludeOptions = {}): Plugin {
   return {
     name: 'vite-plugin-optimize-exclude',
     enforce: 'post',
     async config(config) {
       const result = await scanPackagesList()
+
+      if (options.debug) {
+        console.debug('[Optimize Exclude] ESM packages:')
+        console.debug([...result.esm].map(i => `  ${i}`).join('\n'))
+        console.debug('[Optimize Exclude] Non-ESM packages:')
+        console.debug([...result.nonEsm].map(i => `  ${i}`).join('\n'))
+      }
       const exclude = [...result.esm]
         .filter(name => !config.optimizeDeps?.include?.includes(name))
 
